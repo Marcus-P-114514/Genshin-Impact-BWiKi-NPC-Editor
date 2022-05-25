5175047c919da9b19ac18f6b811e332fb1f26953
Marcus-P-114514 <102861175+Marcus-P-114514@users.noreply.github.com>
2022-05-25 21:36:28 +0800

    Add Support: Store Cache
    That used in store_two.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,6 +9,8 @@
 #include "QSettings"
 #include "QApplication"
 #include "QProcess"
+#include "QFile"
+#include "QTextStream"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -29,6 +31,12 @@ MainWindow::MainWindow(QWidget *parent)
     clear_cache->setValue("basic/org", "");
     clear_cache->setValue("basic/system", "无");
     delete clear_cache;
+
+    QFile clear_store_two_target ("./cache/store_two.npchandler");
+    if (clear_store_two_target.open(QFile::WriteOnly|QFile::Text)){
+        QTextStream clear_store_two (&clear_store_two_target);
+        clear_store_two << "";
+    }
 
     //强制退出
 
