2e096bb35a680abf5a03d2c5bc198e57f7fa0186
Marcus-P-114514 <102861175+Marcus-P-114514@users.noreply.github.com>
2022-06-01 17:42:51 +0800

    Improve UI

--- main.cpp
+++ main.cpp
@@ -3,10 +3,39 @@
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
+#include <QFontDatabase>
+#include <QIODevice>
+#include <QFile>
+
+void FontsSerif() {
+    int index = QFontDatabase::addApplicationFont(":/fonts/resources/fonts/SourceHanSerifSC-Regular.otf");
+    qDebug()<<"index="<<index;
+    if(index != -1) {
+        QStringList fontList(QFontDatabase::applicationFontFamilies(index));
+        if(fontList.count() > 0) {
+            //思源宋体Font Family：Source Han Serif SC
+        }
+    }
+}
+
+void FontsSans() {
+    int index = QFontDatabase::addApplicationFont(":/fonts/resources/fonts/SourceHanSansCN-Regular.otf");
+    qDebug()<<"index="<<index;
+    if(index != -1) {
+        QStringList fontList(QFontDatabase::applicationFontFamilies(index));
+        if(fontList.count() > 0) {
+            //思源黑体Font Family：Source Han Sans CN
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    //Load Fonts
+    FontsSerif();
+    FontsSans();
 
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
